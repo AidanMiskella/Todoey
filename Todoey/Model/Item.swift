b783245357ab5f1de559e6f085290a561f55796b
PPS <8954630c>
2018-08-13 06:55:31 -0700

    Initial Commit

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Aidan miskella on 11/08/2018.
+//  Copyright © 2018 Aidan Miskella. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
